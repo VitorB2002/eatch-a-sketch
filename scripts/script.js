9343d8339082caac4ff464fdd38063147b38b83f
VitorB2002 <vitorb.santos2002@gmail.com>
2022-10-24 12:32:57 -0300

    Add paiting with click

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,14 @@ const container = document.getElementById("grid-container");
 let gridItem = document.createElement("div");
 gridItem.classList.add("grid-item");
 
+function paint(gridItem){
+    gridItem.classList.add("grid-item-painted");
+}
+
 for(i = 0; i < 256 ; i++){
     let gridItemClone = gridItem.cloneNode();
+    gridItemClone.addEventListener("click", () => {
+        paint(gridItemClone);
+    })
     container.append(gridItemClone);
-    console.log("rodei");
 }
