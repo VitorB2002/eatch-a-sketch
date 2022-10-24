90796ca9dba028f16b2919f9f80b94487dd2492d
VitorB2002 <vitorb.santos2002@gmail.com>
2022-10-24 13:38:52 -0300

    Add user input button

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 const container = document.getElementById("grid-container");
+const gridButton = document.getElementById("grid-button");
 let gridItem = document.createElement("div");
+let qtdGrid = 0;
 gridItem.classList.add("grid-item");
 
 function paint(gridItem){
@@ -12,10 +14,30 @@ function mouseHovering(gridItem){
     })
 }
 
-for(i = 0; i < 256 ; i++){
-    let gridItemClone = gridItem.cloneNode();
-    gridItemClone.addEventListener("mouseover", () => {
-        mouseHovering(gridItemClone);
-    })
-    container.append(gridItemClone);
+function createGrid(){
+
+    if(qtdGrid == 0){
+
+        for(i = 0; i < 256 ; i++){
+            let gridItemClone = gridItem.cloneNode();
+            gridItemClone.addEventListener("mouseover", () => {
+                mouseHovering(gridItemClone);
+            })
+            container.append(gridItemClone);
+        }
+
+    }   else{
+    }
+
 }
+
+gridButton.addEventListener("click", () => {
+    qtdGrid = prompt("Choose how many items on the grid (max. 100): ");
+    
+    while(qtdGrid > 100 || qtdGrid <= 0){
+        qtdGrid = prompt("Invalid value, choose again: ");
+    }
+
+});
+
+createGrid();
