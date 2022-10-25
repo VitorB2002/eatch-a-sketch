e644691c34490c080caca26b7e4757e0942dffd5
VitorB2002 <vitorb.santos2002@gmail.com>
2022-10-25 11:18:34 -0300

    Add create grid functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,10 @@
 const container = document.getElementById("grid-container");
 const gridButton = document.getElementById("grid-button");
+const containerWidth = 1200;
 let gridItem = document.createElement("div");
 let qtdGrid = 0;
+let oldQtdGrid
+let itemsWidth;
 gridItem.classList.add("grid-item");
 
 function paint(gridItem){
@@ -14,11 +17,27 @@ function mouseHovering(gridItem){
     })
 }
 
+function resetGrid(){
+
+    if(oldQtdGrid == 0){
+        oldQtdGrid = 16;
+    }
+
+    for(i = 0 ; i < oldQtdGrid*oldQtdGrid ; i++){
+        container.removeChild(container.lastElementChild);
+    }
+}
+
 function createGrid(){
 
     if(qtdGrid == 0){
 
-        for(i = 0; i < 256 ; i++){
+        itemsWidth = containerWidth/16;
+        gridItem.style.width = String(itemsWidth + "px");
+        gridItem.style.height = gridItem.style.width;
+
+        //16x16 grid
+        for(i = 0; i < 16*16 ; i++){
             let gridItemClone = gridItem.cloneNode();
             gridItemClone.addEventListener("mouseover", () => {
                 mouseHovering(gridItemClone);
@@ -27,16 +46,33 @@ function createGrid(){
         }
 
     }   else{
+
+        resetGrid();
+        itemsWidth = containerWidth/qtdGrid;
+        gridItem.style.width = String(itemsWidth + "px");
+        gridItem.style.height = gridItem.style.width;
+
+        for(i = 0; i < qtdGrid*qtdGrid ; i++){
+            let gridItemClone = gridItem.cloneNode();
+            gridItemClone.addEventListener("mouseover", () => {
+                mouseHovering(gridItemClone);
+            })
+            container.append(gridItemClone);
+        }
+
     }
 
 }
 
 gridButton.addEventListener("click", () => {
+    oldQtdGrid = qtdGrid;
     qtdGrid = prompt("Choose how many items on the grid (max. 100): ");
     
     while(qtdGrid > 100 || qtdGrid <= 0){
         qtdGrid = prompt("Invalid value, choose again: ");
     }
+
+    createGrid();
 
 });
 
